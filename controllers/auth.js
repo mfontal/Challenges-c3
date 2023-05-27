47e174e57e73d31a506ac18312b57e3c19408446
mfontal <fontalmilton@gmail.com>
2023-05-26 23:58:24 -0500

    Creacion de carpeta de challenges

---
+++ controllers/auth.js
@@ -0,0 +1,36 @@
+express = require('express');
+const { validationResult } = require('express-validator');
+
+const crearUsuario = (req, res = express.response) => {
+    const {name, email, password}= req.body
+
+    res.status(200).json({
+        ok: true,
+        name: name, 
+        email: email, 
+        password: password
+    });
+};
+
+// Challenge 17
+const loginUsuario = (req, res = express.response) => {
+    const {email, password}= req.body
+    
+    res.status(200).json({
+        ok: true,
+        email,
+        password,
+    })
+}
+
+const revalidarToken = (req, res = express.response) => {
+    res.json({
+        ok: true
+    })
+}
+
+module.exports = {
+    loginUsuario,
+    crearUsuario,
+    revalidarToken
+}
